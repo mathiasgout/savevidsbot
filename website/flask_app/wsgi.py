996e0fbf5599a5137f68d5e0fad8175bf6cbeb66
Mathias Gout <mathias.gout@gmail.com>
2023-02-04 17:17:00 +0100

    prod config

--- website/flask_app/wsgi.py
+++ website/flask_app/wsgi.py
@@ -2,7 +2,7 @@ from app import create_app
 from db.firebase_db import init_firebase_app
 
 
-app = create_app(config="DevConfig")
+app = create_app(config="ProdConfig")
 init_firebase_app()
 
 if __name__ == "__main__":
